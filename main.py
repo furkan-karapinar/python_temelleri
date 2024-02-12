2123ea93aa5ddc8b0dbb0213a73022368c2f0f75
Furkan <furkankarapinar.2003@gmail.com>
2024-02-12 15:10:15 +0300

    f

---
+++ main.py
@@ -0,0 +1,36 @@
+#: Yorum Satırı
+
+"""
+Kısayollar
+
+Ctrl + D -> Seçilen satırı alt satıra kopyalar
+Ctrl + / -> Seçilen satırı yorum satırı yapar
+Ctrl + W -> Bloklar halinde seçilen satırdan katman katman ilerler
+
+Ctrl + Space -> Intellisence çalıştırır
+Alt + Down / UP -> Methodlar arası geçiş yapar
+Ctrl + L -> Kod yazımını düzenler
+
+"""
+
+# Ders-1 Değişkenler
+
+var1 = 10 #Integer
+print(type(var1))
+
+var2 = "Pazartesi" #String
+print(type(var2))
+
+var3 = 1.3 #Float
+print(type(var3))
+
+sonuc = var1 + var3
+print(sonuc)
+print(type(sonuc))
+
+
+# Örnek
+rakam1 = "100"
+rakam2 = "200"
+print(rakam1 + rakam2)
+print(int(rakam1) + int(rakam2))
